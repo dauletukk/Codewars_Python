4322aad71d72f3416b6b2e5e1b044357ba278316
dauletukk <Alana1507>
2020-07-31 11:54:41 -0700

    Do I get a bonus?

---
+++ 8 Do I get a bonus.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
+def bonus_time(salary, bonus):
+    if bonus:
+        salary = salary * 10
+    x = str(salary)
+    return '$'+x
+    #your code here
